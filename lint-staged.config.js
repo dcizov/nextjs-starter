14e6e39ca675c050419e9b0070a1ba081e361df3
dcizov <dimitricizov30@gmail.com>
2025-10-01 17:01:41 +0200

    chore(config): unify linting scripts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,5 @@
 /** @type {import('lint-staged').Configuration} */
 const lintStagedConfig = {
-  // Format + lint staged code files (JS + TS). lint-staged appends files automatically.
   "**/*.{ts,tsx,js,jsx}": [
     "prettier --write --cache",
     "eslint --fix --cache",
