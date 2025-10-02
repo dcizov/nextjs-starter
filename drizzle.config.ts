d1c893ccb824af7ef518a6fd89a3ce6fc92c0d2e
dcizov <dimitricizov30@gmail.com>
2025-10-02 03:42:33 +0200

    refactor(db): migrate drizzle config to defineconfig api

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,9 @@
-import { type Config } from "drizzle-kit";
-
+import { defineConfig } from "drizzle-kit";
 import { env } from "@/env";
 
-export default {
-  schema: "./src/server/db/schema.ts",
+export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
   },
-} satisfies Config;
+});
